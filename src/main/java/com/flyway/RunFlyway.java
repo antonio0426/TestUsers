01b041d37f79c323debca4f689e9062b947e99a7
ant.mangano0426 <ant.mangano@gmail.com>
2022-06-17 16:24:28 +0200

    commit

--- src/main/java/com/flyway/RunFlyway.java
+++ src/main/java/com/flyway/RunFlyway.java
@@ -20,10 +20,10 @@ public class RunFlyway {
 	String datasourceUsername;
 	@ConfigProperty(name = "quarkus.datasource.password")
 	String datasourcePassword;
-	
+
 	public void runFlywayMigration(@Observes StartupEvent event) {
 		if (runMigration) {
-			Flyway flyway = Flyway.configure().
+			Flyway flyway = Flyway.configure()
 					.dataSource("jdbc:" + datasourceUrl, datasourceUsername, datasourcePassword).load();
 			flyway.migrate();
 		}
